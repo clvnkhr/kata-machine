2aed4b0bccf6fc2bdc48c81430be8b5feb3ecd3f
clvnkhr <calvin_khor@hotmail.com>
2023-06-25 12:48:53 +0800

    pr from parent, add: twocrystalball - case of perfect sqrts #37

--- src/__tests__/TwoCrystalBalls.ts
+++ src/__tests__/TwoCrystalBalls.ts
@@ -12,5 +12,17 @@ test("two crystal balls", function() {
     expect(two_crystal_balls(new Array(821).fill(false))).toEqual(-1);
     expect(two_crystal_balls(new Array(821).fill(true))).toEqual(0);
     expect(two_crystal_balls(new Array(821).fill(false).concat(true))).toEqual(821);
+
+    // for testing exact sqrts
+    let sqrti = Math.floor(Math.random() * 99 + 1); // num > 0
+    let dataSize = sqrti * sqrti;
+    const sqrtsData = new Array(dataSize).fill(false);
+
+    for (let i = sqrti; i < dataSize; i++) {
+        sqrtsData[i] = true;
+    }
+
+    expect(two_crystal_balls(sqrtsData)).toEqual(sqrti);
+    expect(two_crystal_balls([])).toEqual(-1);
 });
 
