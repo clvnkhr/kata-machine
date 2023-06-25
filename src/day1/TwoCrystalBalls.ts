040d1d6e67d8f5c5adb5f3ded812031aae81e74f
clvnkhr <calvin_khor@hotmail.com>
2023-06-25 13:05:20 +0800

    fixed solution using new tests

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,19 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-    const jump = Math.floor(Math.sqrt(breaks.length))
+    const jump = Math.floor(Math.sqrt(breaks.length));
 
-    let firstBreak = -1
+    let firstBreak = -1;
     for (let i = jump; i < breaks.length; i = i + jump) {
         if (breaks[i]) {
             firstBreak = i;
             break;
         }
     }
+    // off-by-one type error
+    if (firstBreak === -1 && breaks[breaks.length - 1]) {
+        return breaks.length - 1
+    }
     if (firstBreak > -1) {
         for (let i = firstBreak - jump; i <= firstBreak; i++) {
             if (breaks[i]) {
-                return i
+                return i;
             }
         }
     }
-    return -1
+    return -1;
 }
